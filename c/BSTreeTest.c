b62b4364d001c9d4aae6f2fa981686aaef469800
Joshua Bartlett <jcbart12595@gmail.com>
2017-11-28 13:42:54 -0500

    starting on pretty print

--- c/BSTreeTest.c
+++ c/BSTreeTest.c
@@ -5,12 +5,10 @@
 int main()
 {
     struct BSTreeNode *root = createNode(5);
-    addElementToTree(root, 2);
+    addElementToTree(root, 3);
     addElementToTree(root, 4);
-    addElementToTree(root, 1);
     printTree(root);
-    free(root->left->left);
-    free(root->left->right);
+    free(root->right);
     free(root->left);
     free(root);
 }
