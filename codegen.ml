fa9db5df0310a76133e1b7f61ec446350234e8c1
sophstad <srs2231@columbia.edu>
2017-12-01 00:38:24 +0000

    Implement and/or

--- codegen.ml
+++ codegen.ml
@@ -272,6 +272,8 @@ and translate (globals, functions) =
           | A.Mult    -> L.build_fmul
           | A.Div     -> L.build_fdiv
           | A.Mod     -> L.build_frem
+          | A.And     -> L.build_and
+          | A.Or      -> L.build_or
           | A.Equal   -> L.build_fcmp L.Fcmp.Oeq
           | A.Neq     -> L.build_fcmp L.Fcmp.One
           | A.Less    -> L.build_fcmp L.Fcmp.Ult
@@ -286,6 +288,8 @@ and translate (globals, functions) =
           | A.Mult   -> L.build_mul
           | A.Div  -> L.build_sdiv
           | A.Mod     -> L.build_srem
+          | A.And     -> L.build_and
+          | A.Or      -> L.build_or
           | A.Equal   -> L.build_icmp L.Icmp.Eq
           | A.Neq     -> L.build_icmp L.Icmp.Ne
           | A.Less    -> L.build_icmp L.Icmp.Slt
